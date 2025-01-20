128ff1717182d8d4d5d898bd94db72ce9dda9e1f
AlejandroIR <shibainuale@gmail.com>
2025-01-20 19:16:22 +0100

    AIR - Create / Destroy collision objects + collision shape types +

--- CoffeeEngine/src/CoffeeEngine/Physics/PhysicsEngine.cpp
+++ CoffeeEngine/src/CoffeeEngine/Physics/PhysicsEngine.cpp
@@ -8,6 +8,8 @@ namespace Coffee {
     btBroadphaseInterface* PhysicsEngine::broad_phase = nullptr;
     btConstraintSolver* PhysicsEngine::solver = nullptr;
     btVehicleRaycaster* PhysicsEngine::vehicle_raycaster = nullptr;
+    std::vector<btCollisionObject*> PhysicsEngine::m_CollisionObjects;
+    std::vector<btCollisionShape*> PhysicsEngine::m_CollisionShapes;
 
     void PhysicsEngine::Init()
     {
@@ -31,6 +33,19 @@ namespace Coffee {
 
     void PhysicsEngine::Destroy()
     {
+        for (auto* obj : m_CollisionObjects)
+        {
+            dynamicsWorld->removeCollisionObject(obj);
+            delete obj;
+        }
+        m_CollisionObjects.clear();
+
+        for (auto* shape : m_CollisionShapes)
+        {
+            delete shape;
+        }
+        m_CollisionShapes.clear();
+
         delete dynamicsWorld;
         delete solver;
         delete broad_phase;
@@ -71,5 +86,95 @@ namespace Coffee {
     glm::vec3 PhysicsEngine::BulletToGlm(const btVector3& v)
     {
         return glm::vec3(v.x(), v.y(), v.z());
+    }
+
+    btCollisionShape* PhysicsEngine::CreateCollisionShape(const CollisionShapeConfig& config)
+    {
+        btCollisionShape* shape = nullptr;
+        
+        switch (config.type)
+        {
+            case CollisionShapeType::BOX:
+                shape = new btBoxShape(GlmToBullet(config.size * 0.5f));
+                break;
+            case CollisionShapeType::SPHERE:
+                shape = new btSphereShape(config.size.x);
+                break;
+            case CollisionShapeType::CAPSULE:
+                shape = new btCapsuleShape(config.size.x, config.size.y);
+                break;
+            case CollisionShapeType::CYLINDER:
+                shape = new btCylinderShape(GlmToBullet(config.size));
+                break;
+            default:
+                shape = new btBoxShape(GlmToBullet(config.size * 0.5f));
+                break;
+        }
+
+        m_CollisionShapes.push_back(shape);
+        return shape;
+    }
+
+    btCollisionObject* PhysicsEngine::CreateCollisionObject(const CollisionShapeConfig& config, const glm::vec3& position)
+    {
+        btCollisionShape* shape = CreateCollisionShape(config);
+        
+        btCollisionObject* object = nullptr;
+        
+        if (config.isStatic || config.isTrigger)
+        {
+            object = new btCollisionObject();
+        }
+        else
+        {
+            btVector3 localInertia(0, 0, 0);
+            if (!config.isStatic)
+                shape->calculateLocalInertia(config.mass, localInertia);
+
+            btDefaultMotionState* motionState = new btDefaultMotionState(
+                btTransform(btQuaternion(0, 0, 0, 1), GlmToBullet(position))
+            );
+
+            btRigidBody::btRigidBodyConstructionInfo rbInfo(
+                config.mass, motionState, shape, localInertia
+            );
+
+            object = new btRigidBody(rbInfo);
+        }
+
+        object->setCollisionShape(shape);
+        object->setWorldTransform(btTransform(btQuaternion(0, 0, 0, 1), GlmToBullet(position)));
+
+        if (config.isTrigger)
+        {
+            object->setCollisionFlags(object->getCollisionFlags() | btCollisionObject::CF_NO_CONTACT_RESPONSE);
+        }
+
+        dynamicsWorld->addCollisionObject(object);
+        m_CollisionObjects.push_back(object);
+
+        return object;
+    }
+
+    void PhysicsEngine::DestroyCollisionObject(btCollisionObject* object)
+    {
+        if (!object) return;
+
+        dynamicsWorld->removeCollisionObject(object);
+        
+        auto it = std::find(m_CollisionObjects.begin(), m_CollisionObjects.end(), object);
+        if (it != m_CollisionObjects.end())
+        {
+            m_CollisionObjects.erase(it);
+        }
+
+        // if rigid body, delete motion state
+        btRigidBody* body = btRigidBody::upcast(object);
+        if (body && body->getMotionState())
+        {
+            delete body->getMotionState();
+        }
+
+        delete object;
     }
 }
--- CoffeeEngine/src/CoffeeEngine/Physics/PhysicsEngine.h
+++ CoffeeEngine/src/CoffeeEngine/Physics/PhysicsEngine.h
@@ -1,9 +1,9 @@
 #pragma once
 #include <bullet/btBulletDynamicsCommon.h>
 #include <glm/glm.hpp>
+#include <vector>
 
 namespace Coffee{
-
 
     enum class PhysicsType
     {
@@ -12,7 +12,24 @@ namespace Coffee{
         PARALLEL,
         CONTINUOUS
     };
-    
+
+    // collision shape types
+    enum class CollisionShapeType {
+        BOX,
+        SPHERE,
+        CAPSULE,
+        CYLINDER,
+        MESH
+    };
+
+    struct CollisionShapeConfig {
+        CollisionShapeType type = CollisionShapeType::BOX;
+        glm::vec3 size = glm::vec3(1.0f);  // size of the shape
+        bool isTrigger = false;
+        float mass = 1.0f;
+        bool isStatic = false;
+    };
+
     static class PhysicsEngine {
     public:
         static void Init();
@@ -27,10 +44,15 @@ namespace Coffee{
         static btVector3 GlmToBullet(const glm::vec3& v);
         static glm::vec3 BulletToGlm(const btVector3& v);
 
+        // Create + Destroy collision objects
+        static btCollisionObject* CreateCollisionObject(const CollisionShapeConfig& config, const glm::vec3& position);
+        static void DestroyCollisionObject(btCollisionObject* object);
+        
+        static btCollisionShape* CreateCollisionShape(const CollisionShapeConfig& config);
+
     private:
 
         static btDynamicsWorld* dynamicsWorld;
-
 
         static btCollisionConfiguration*	collision_conf;
         static btDispatcher*				dispatcher;
@@ -38,6 +60,9 @@ namespace Coffee{
         static btConstraintSolver*          solver;
         static btVehicleRaycaster*			vehicle_raycaster;
         //static DebugDrawer*				debug_draw;
+
+        static std::vector<btCollisionObject*> m_CollisionObjects;
+        static std::vector<btCollisionShape*> m_CollisionShapes;
     };
 
 }
