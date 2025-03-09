0b7ba81ebc502ae43c000e1d91a750510ab09f2f
Hugo Planell <hugoplanellmoreno@gmail.com>
2025-03-09 01:33:24 +0100

    ImportPanel updated to the ImportData new system

--- CoffeeEditor/Panels/ImportPanel.h
+++ CoffeeEditor/Panels/ImportPanel.h
@@ -6,26 +6,16 @@ namespace Coffee {
 
     class ImportPanel : public Panel
     {
-        public:
-            ImportPanel() = default;
-            ImportPanel(const Ref<Scene>& scene);
+    public:
+        ImportPanel() = default;
+        ImportPanel(const Ref<Scene>& scene);
 
-            void SetContext(const Ref<Scene>& scene);
-
-            void OnImGuiRender() override;
-
-            Ref<Resource> selectedResource;
-
-        private:
-            Ref<Scene> m_Context;
-
-            // TODO remove temp mockup variables
-            bool srgb = false;
-            bool flipY = false;
-            bool flipX = false;
-            float globalScale;
-            bool ignoreLight = false;
-            bool ignoreCamera = false;
+        void OnImGuiRender() override;
+        void SetSelectedResource(const Ref<Resource>& resource) { m_SelectedResource = resource; }
+    private:
+        Ref<Resource> m_SelectedResource; // Think if this should be a Ref
+        Ref<Resource> m_LastSelectedResource;
+        Scope<ImportData> m_CachedImportData;
     };
 
 }
